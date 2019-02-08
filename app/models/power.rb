56ad67bef9267b62bd0455aebb5b0e4f60f3d9b5
github username <github email address>
2019-02-08 10:56:04 -0500

    everything exempt bonus which is almost done but not quite there yet

--- app/models/power.rb
+++ app/models/power.rb
@@ -1,2 +1,11 @@
 class Power < ApplicationRecord
+  has_many :heroines
+
+  def self.search(search)
+  if search
+    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+  else
+    ""
+  end
+end
 end
