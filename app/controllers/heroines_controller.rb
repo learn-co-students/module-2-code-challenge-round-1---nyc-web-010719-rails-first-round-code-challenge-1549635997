6f4228a1c39e287f9f438868cafc736aa2b57a8a
github username <github email address>
2019-02-08 11:12:37 -0500

    challenge accepted

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,47 @@
 class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
+
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @power = @heroine.power
+  end
+
+  def new
+    @heroine = Heroine.new
+    # redirect_to new_heroine_path
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    @heroine.save
+    redirect_to @heroine
+  end
+
+  def search
+    @power = Heroine.power
+    if params[:search] == "super strength"
+  
+    elsif params[:search] == "flight"
+
+    elsif params[:search] == "super human senses"
+
+    elsif params[:search] == "elasticty"
+
+    else
+      render :index
+    end
+
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :powers, only: [:index, :show]
+  resources :heroines, only: [:index, :show, :new, :create]
+
+
+  post '/heroines/new', to: 'heroines#create'
+  get '/search', to: "heroines#index"
 end
