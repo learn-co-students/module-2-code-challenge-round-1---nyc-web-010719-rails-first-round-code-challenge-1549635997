7049353d3bf466eafcff821349bfa0c3bab5b467
cvolpe12 <christopher.volpe12@gmail.com>
2019-02-08 10:51:18 -0500

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,37 @@
 class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
+
+    if params[:super_power]
+      @heroines = @heroines.select do |heroine|
+        heroine.power.name == params[:super_power]
+      end
+      @heroines
+    end
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(strong_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      redirect_to new_heroine_path
+    end
+  end
+
+
+  private
+  def strong_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
