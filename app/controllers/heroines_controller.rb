f78f4edc800386dc66b26ccb56810bb88502105e
davidfloyd91 <davidfloyd91@gmail.com>
2019-02-08 10:41:23 -0500

    Done.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,38 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    if params[:term]
+      @power_search = Power.find_by(name: params[:term]).id
+      @heroines = Heroine.where('power_id LIKE ?', "#{@power_search}")
+    else
+      @heroines = Heroine.all
+    end
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+
+    if @heroine.valid?
+      @heroine.save
+      flash[:notice] = "#{@heroine.super_name} has been supersaved!"
+      redirect_to @heroine
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id, :term)
   end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,4 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+  validates :super_name, uniqueness: { message: "Please choose another supername, that one's supertaken." }
 end
