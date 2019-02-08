4d93d52ef50749374475ec177b018a64b8e1f941
davidfloyd91 <davidfloyd91@gmail.com>
2019-02-08 10:45:52 -0500

    I mean it this time. Done.

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,4 +1,4 @@
 class Heroine < ApplicationRecord
   belongs_to :power
-  validates :super_name, uniqueness: { message: "Please choose another supername, that one's supertaken." }
+  validates :super_name, uniqueness: { message: " error: Please choose another supername, that one's supertaken." }
 end
