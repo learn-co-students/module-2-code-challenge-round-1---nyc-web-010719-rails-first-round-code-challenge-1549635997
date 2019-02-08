2ebcb95d3c01fda76caa051ec2a88fd3953a90d7
purplesands <mullenjoe13@gmail.com>
2019-02-08 10:23:10 -0500

    MVP

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,32 @@
 class HeroinesController < ApplicationController
+  
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new(params[:id])
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+       @heroine.save
+       redirect_to @heroine
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,5 +1,15 @@
 class PowersController < ApplicationController
+  
   def index
     @powers = Power.all
   end
+
+  def new
+    @power = Power.new(params[:id])
+  end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
 end
